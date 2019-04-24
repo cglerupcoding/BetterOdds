d8e3fcf2ac31eb49cb13e2b7c0a51f04c8d911b4
Cameron <cameronleecalder@gmail.com>
2019-04-23 19:29:37 -0500

    201904231929 commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -25,6 +25,9 @@
 
 //apikey = d38373eaef05ac04f5bc305caee2257c
 // function sportsOdds(){
+var team1Odds;
+var team2Odds;
+
   var apiData =
   "https://api.the-odds-api.com/v3/odds?apiKey=d38373eaef05ac04f5bc305caee2257c&region=uk&sport=icehockey_nhl";
 $.ajax({
@@ -32,7 +35,12 @@ $.ajax({
   method: "GET"
 }).then(function(response) {
   console.log(response);
-  console.log(response.data["7"].details);
+  console.log(response.data[0].sites[3].odds.h2h[0]);
+  console.log(response.data[0].sites[3].odds.h2h[1]);
+  team1Odds = response.data[0].sites[3].odds.h2h[0];
+  team2Odds = response.data[0].sites[3].odds.h2h[1];
+
+  return team1Odds, team2Odds;
 });
 // }
 
