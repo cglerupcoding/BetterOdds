a303d48d2b62c383709ac9e24b5c14cc0350239e
Bruce Van Horn <brucevanhorn@gmail.com>
2019-04-24 21:06:53 -0500

    [#6] cleaned a few things up then got firebase auth to register new users.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,46 +1,15 @@
-// Carousel
-// $('#myCarousel').carousel({
-//     interval: 2000,
-//     wrap: true
-//       })
-
-
-// My sports feed api
-
-// var apiKey = "ae1a5593-8260-45b1-a132-982ff4"
-// $.ajax({
-//  type: "GET",
-//  url: "https://api.mysportsfeeds.com/v2.1/pull/nhl/2019-playoff/games.json",
-//  dataType: 'json',
-//  async: false,
-//  headers: {
-//    "Authorization": "Basic " + btoa(apiKey + ":" + MYSPORTSFEEDS)
-//  },
-//  data: '{ "comment" }',
-//  success: function (){
-//    alert('Thanks for your comment!');
-//  }
-// });
-
-
-//apikey = d38373eaef05ac04f5bc305caee2257c
-
-
-
 var oddsData = [];
-function sportsOdds(){
-// >>>>>>> 796e849552861a2de2a4e1617d9b1a6c4de96e9b
+function sportsOdds() {
   var apiData =
-  "https://api.the-odds-api.com/v3/odds?apiKey=d38373eaef05ac04f5bc305caee2257c&region=uk&sport=icehockey_nhl";
-$.ajax({
-  url: apiData,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-  //console.log(response);
-  oddsData = response.data;
-  console.log(oddsData.0.data);
-});
+    "https://api.the-odds-api.com/v3/odds?apiKey=d38373eaef05ac04f5bc305caee2257c&region=uk&sport=icehockey_nhl";
+  $.ajax({
+    url: apiData,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    //console.log(response);
+    oddsData = response.data;
+  });
 }
 
 sportsOdds();  //this would be better as an onload event for the window. (BVH)
@@ -60,26 +29,28 @@ var dataRef = firebase.database();
 //events handlers
 
 //login button click handler
-$("#signUpSubmitBtn").on("click", function(event){
+$("#signUpSubmitBtn").on("click", function (event) {
   event.preventDefault();
   //TODO:  validate thoroughly
-    //do the passwords match?
-    //is the email field a valid address?
-    //does the user already exist?
+  //do the passwords match?
+  //is the email field a valid address?
+  //does the user already exist?
 
-  var newUser = {};
-  newUser.email = $("#enterEmail").val.trim();
-  newUser.password = $("#enterPassword").val.trim();
-  dataRef("/users").push(newUser);
+  var userEmail = $("#enterEmail").val().trim();
+  var userPassword = $("#enterPassword").val().trim();
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
+  .catch(function(error){
+    console.log(error.message);
+  });
 });
 
 
-function showSignupForm(event){
+function showSignupForm(event) {
   event.preventDefault();
   $("#modalSignupForm").show();
 }
 
-function hideSignupForm(event){
+function hideSignupForm(event) {
   event.preventDefault();
   //then hide it
   $("#modalSignupForm").hide();
@@ -102,15 +73,15 @@ var dataSet = [{
 
 // 
 // var data = {
-    // name = SHIT FROM THE API hRERE;
-    // odds = SHIT FROM THE API hRERE;
-    // score = SHIT FROM THE API hRERE;
+// name = SHIT FROM THE API hRERE;
+// odds = SHIT FROM THE API hRERE;
+// score = SHIT FROM THE API hRERE;
 //}
 
 //dataSet = $.parseJSON(dataSet);
 
-$(function populateOddsTable() {
-  $.each(dataSet, function(i, item) {
+function populateOddsTable() {
+  $.each(dataSet, function (i, item) {
     var $tr = $('<tr>').append(
       $('<td>').text(item.name),
       $('<td>').text(item.odds),
@@ -119,10 +90,6 @@ $(function populateOddsTable() {
     console.log($tr.wrap('<p>').html());
     $("#oddsTable > tbody").append($tr);
   });
-});
 
+}
 populateOddsTable();
-
-// $(function calculateBet() {
-//   result = userWager * item.odds;
-// })
