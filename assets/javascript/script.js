07c75a08d11aa76a2d80681b8cc39c6b5c570e6e
Bruce Van Horn <brucevanhorn@gmail.com>
2019-04-24 21:34:25 -0500

    added validation logic and message control

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,6 +26,12 @@ var config = {
 firebase.initializeApp(config);
 var dataRef = firebase.database();
 
+//utility functions
+function isValidEmailAddress(emailAddress) {
+  var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+  return pattern.test(emailAddress);
+}
+
 //events handlers
 
 //login button click handler
@@ -35,13 +41,46 @@ $("#signUpSubmitBtn").on("click", function (event) {
   //do the passwords match?
   //is the email field a valid address?
   //does the user already exist?
-
+  var valid = true;
   var userEmail = $("#enterEmail").val().trim();
   var userPassword = $("#enterPassword").val().trim();
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
-  .catch(function(error){
-    console.log(error.message);
-  });
+  var userPasswordRepeat = $("#enterPasswordRepeat").val().trim();
+
+  if (userPassword !== userPasswordRepeat) {
+    $("#signupFailMessage").text("The two passwords must match.  They don't.");
+    valid = false;
+  }
+
+  if (userPassword.length < 6) {
+    $("#signupFailMessage").text("The password needs to be at least 6 characters long.");
+    valid = false;
+  }
+
+  if (!isValidEmailAddress(userEmail)) {
+    $("#signupFailMessage").text("The email address format is not valid.");
+    valid = false;
+  }
+
+  // if everything is valid, send it over
+  if (valid) {
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
+      .catch(function (error) {
+        console.log(error.message);
+        valid = false;
+        $("#signupFailMessage").text(error.message);
+      });
+  }
+
+  //firebase can still reject so it too triggers valid = false and sets the message.
+  //nothing left but to show the appropriate message
+  if(valid){
+    $("#signupSuccessMessage").show();
+    $("#signupFailMessage").hide();
+  } else {
+    $("#signupSuccessMessage").hide();
+    $("#signupFailMessage").show();
+  }
+
 });
 
 
@@ -56,6 +95,8 @@ function hideSignupForm(event) {
   $("#modalSignupForm").hide();
 }
 
+$("#signupSuccessMessage").hide();
+$("#signupFailMessage").hide();
 $("#showSignUpModal").on("click", showSignupForm);
 $("#signUpCancelBtn").on("click", hideSignupForm);
 
