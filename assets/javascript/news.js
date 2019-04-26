e29225e6f39b8598ab41ebb0765b38da53202504
Chris Glerup <cglerupcoding@gmail.com>
2019-04-25 23:03:30 -0500

    API is working for News, I still to build the loops for images and links

--- assets/javascript/news.js
+++ assets/javascript/news.js
@@ -5,17 +5,28 @@
     url: newsData,
     method: "GET"
   }).then(function (response) {
-    console.log(response);
+    // console.log(response);
     newsData = response;
-      $.each(newsData.data, function (i, item) {
-        var $tr = $('<tr>').append(
-          $('#newsOne').text(newsData.data[i].teams),
-          $('#newsTwo').text(newsData.data[i].sites[2].news.h2h),
-          $('#newsThree').text(newsData.data[i].sites[2].news.h2h)
-        );
-        console.log($tr.wrap('<p>').html());
-        $("#newsTable > tbody").append($tr);
+      // $.for(newsData.results, function (i, item) {
+        $('.container').append
+      for (i  =0; i < 2; i++) {
+        console.log(response);
+
+
+         // Creating and storing a div tag
+         var container = $("<div>");
+        var h1 = $("<h1>").text(JSON.stringify(newsData.results[i].title));
+
+          // $('#newsOne').text(JSON.stringify(newsData.results[i]));
+          // $('#newsTwo').text(newsData.data[i].sites[2].news.h2h),
+          // $('#newsThree').text(newsData.data[i].sites[2].news.h2h)
+                
+         // Appending the paragraph and image tag to the animalDiv
+         container.append(h1);
+        };
+        // // console.log($tr.wrap('<p>').html());
+        $(".container").append(container);
     });
-  });
+  // });
 
 
